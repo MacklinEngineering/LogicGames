7af7e6e377fe5b29cea4be20ede3d7e6634fd02c
resilient-coders30 <resilient-coders30@resilient-coders30s-MacBook-Pro.local>
2020-04-11 13:20:01 -0400

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,18 @@
+function displayCode(){
+  var x = document.getElementById("code");
+  if (x.style.display === "none"){
+    x.style.display= "block";
+  }else{
+    x.style.display = "none";
+  }
+}
+
+
+function displayOutput(){
+  var x = document.getElementById("output");
+  if (x.style.display === "none"){
+    x.style.display= "block";
+  }else{
+    x.style.display = "none";
+  }
+}
